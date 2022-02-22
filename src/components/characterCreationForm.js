de076a664018947f5d2a5ed893b46e657b2e2f2b
John Laubscher <jlmemphiscommanders@gmail.com>
2022-02-21 22:57:52 -0600

    added matui, configured CharacterCreationForm to app.js

---
+++ src/components/characterCreationForm.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+export const CharacterCreationForm = (props) => {
+  const [characterInfo, setCharacterInfo] = useState("");
+};
+export default CharacterCreationForm;
