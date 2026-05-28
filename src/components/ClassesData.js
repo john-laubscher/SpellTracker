45c9e8b3a93d9ec651c8cb64b392e6b0d5459d0f
John Laubscher <jlmemphiscommanders@gmail.com>
2026-05-27 23:26:22 -0500

    Add Rogue Thief subclass descriptions

--- src/components/ClassesData.js
+++ src/components/ClassesData.js
@@ -5617,39 +5617,52 @@
       },
       
       thief: {
+        // Source (feature text): https://dnd5e.wikidot.com/rogue:thief (CC BY-SA 3.0)
+        name: "Thief",
         features: [
           {
             id: "fast_hands",
             name: "Fast Hands",
-            desc: "Starting at 3rd level, you can use the Use an Object action as a bonus action. Additionally, you can use your thieves' tools to disarm traps or open locks as a bonus action.",
+            desc: [
+              "Starting at 3rd level, you can use the bonus action granted by your Cunning Action to make a Dexterity (Sleight of Hand) check, use your thieves' tools to disarm a trap or open a lock, or take the Use an Object action.",
+            ],
             level: 3,
             tracked: false,
           },
           {
             id: "second_story_work",
             name: "Second-Story Work",
-            desc: "At 3rd level, climbing no longer costs you extra movement. Additionally, you can make running jumps farther than normal, adding your Dexterity modifier to the distance covered.",
+            desc: [
+              "When you choose this archetype at 3rd level, you gain the ability to climb faster than normal; climbing no longer costs you extra movement.",
+              "In addition, when you make a running jump, the distance you cover increases by a number of feet equal to your Dexterity modifier.",
+            ],
             level: 3,
             tracked: false,
           },
           {
             id: "supreme_sneak",
             name: "Supreme Sneak",
-            desc: "At 9th level, you have advantage on Stealth checks if you move no more than half your speed on the same turn.",
+            desc: [
+              "Starting at 9th level, you have advantage on a Dexterity (Stealth) check if you move no more than half your speed on the same turn.",
+            ],
             level: 9,
             tracked: false,
           },
           {
             id: "use_magic_device",
             name: "Use Magic Device",
-            desc: "At 13th level, you can ignore all class, race, and level requirements on the use of magical items.",
+            desc: [
+              "By 13th level, you have learned enough about the workings of magic that you can improvise the use of items even when they are not intended for you. You ignore all class, race, and level requirements on the use of magic items.",
+            ],
             level: 13,
             tracked: false,
           },
           {
             id: "thief's_reflexes",
             name: "Thief's Reflexes",
-            desc: "At 17th level, you can take two turns during the first round of any combat. You take your first turn at your normal initiative and your second turn at your initiative minus 10.",
+            desc: [
+              "When you reach 17th level, you have become adept at laying ambushes and quickly escaping danger. You can take two turns during the first round of any combat. You take your first turn at your normal initiative and your second turn at your initiative minus 10. You can't use this feature when you are surprised.",
+            ],
             level: 17,
             tracked: false,
           },
