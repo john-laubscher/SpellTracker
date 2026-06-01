8984ee4afb304bda0be7cc47a9ddfa74ae8032dc
John Laubscher <jlmemphiscommanders@gmail.com>
2026-06-01 16:34:09 -0500

    Add pyromancy sorcerer subclass

--- src/components/ClassesData.js
+++ src/components/ClassesData.js
@@ -6339,7 +6339,52 @@
           },
         ],
       },
-      Pyromancy: "",
+      Pyromancy: {
+        name: "Pyromancy",
+        source: "Plane Shift: Kaladesh",
+        features: [
+          {
+            id: "heart_of_fire",
+            name: "Heart of Fire",
+            desc: [
+              "At 1st level, whenever you start casting a spell of 1st level or higher that deals fire damage, fiery magic erupts from you.",
+              "This eruption causes creatures of your choice that you can see within 10 feet of you to take fire damage equal to half your sorcerer level (minimum of 1).",
+            ],
+            level: 1,
+            tracked: false,
+          },
+          {
+            id: "fire_in_the_veins",
+            name: "Fire in the Veins",
+            desc: [
+              "At 6th level, you gain resistance to fire damage.",
+              "In addition, spells you cast ignore resistance to fire damage.",
+            ],
+            level: 6,
+            tracked: false,
+          },
+          {
+            id: "pyromancers_fury",
+            name: "Pyromancer's Fury",
+            desc: [
+              "Starting at 14th level, when you are hit by a melee attack, you can use your reaction to deal fire damage to the attacker.",
+              "The damage equals your sorcerer level, and ignores resistance to fire damage.",
+            ],
+            level: 14,
+            tracked: false,
+          },
+          {
+            id: "fiery_soul",
+            name: "Fiery Soul",
+            desc: [
+              "At 18th level, you gain immunity to fire damage.",
+              "In addition, any spell or effect you create ignores resistance to fire damage and treats immunity to fire damage as resistance to fire damage.",
+            ],
+            level: 18,
+            tracked: false,
+          },
+        ],
+      },
     },
   },
   warlock: {
