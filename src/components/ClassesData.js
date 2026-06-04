cac23b95fe9ce8891ba967e9ee189a8dc61ee48d
John Laubscher <jlmemphiscommanders@gmail.com>
2026-06-04 17:19:50 -0500

    Add Hexblade warlock subclass

--- src/components/ClassesData.js
+++ src/components/ClassesData.js
@@ -6851,7 +6851,76 @@ export const ClassesData = {
           { spellLevel: 5, spells: ["dominate-person", "telekinesis"] },
         ],
       },
-      hexblade: "",
+      hexblade: {
+        // Source (feature text): https://dnd5e.wikidot.com/warlock:hexblade (CC BY-SA 3.0)
+        name: "The Hexblade",
+        source: "Xanathar's Guide to Everything",
+        features: [
+          {
+            id: "hexblades_curse",
+            name: "Hexblade's Curse",
+            desc: [
+              "Starting at 1st level, you gain the ability to place a baleful curse on someone. As a bonus action, choose one creature you can see within 30 feet of you. The target is cursed for 1 minute. The curse ends early if the target dies, you die, or you are incapacitated.",
+              "Until the curse ends, you gain the following benefits: you gain a bonus to damage rolls against the cursed target equal to your proficiency bonus; any attack roll you make against the cursed target is a critical hit on a roll of 19 or 20 on the d20; and if the cursed target dies, you regain hit points equal to your warlock level + your Charisma modifier (minimum of 1 hit point).",
+              "Once you use this feature, you can't use it again until you finish a short or long rest.",
+            ],
+            level: 1,
+            tracked: true,
+            uses: 1,
+            recharge: "sr_or_lr",
+          },
+          {
+            id: "hex_warrior",
+            name: "Hex Warrior",
+            desc: [
+              "At 1st level, you acquire the training necessary to effectively arm yourself for battle. You gain proficiency with medium armor, shields, and martial weapons.",
+              "The influence of your patron also allows you to mystically channel your will through a particular weapon. Whenever you finish a long rest, you can touch one weapon that you are proficient with and that lacks the two-handed property. When you attack with that weapon, you can use your Charisma modifier, instead of Strength or Dexterity, for the attack and damage rolls.",
+              "This benefit lasts until you finish a long rest. If you later gain the Pact of the Blade feature, this benefit extends to every pact weapon you conjure with that feature, no matter the weapon's type.",
+            ],
+            level: 1,
+            tracked: false,
+          },
+          {
+            id: "accursed_specter",
+            name: "Accursed Specter",
+            desc: [
+              "Starting at 6th level, you can curse the soul of a person you slay, temporarily binding it in your service. When you slay a humanoid, you can cause its spirit to rise from its corpse as a specter, the statistics of which are in the Monster Manual.",
+              "When the specter appears, it gains temporary hit points equal to half your warlock level. Roll initiative for the specter, which has its own turns. It obeys your verbal commands, and it gains a special bonus to its attack rolls equal to your Charisma modifier (minimum of +0).",
+              "The specter remains in your service until the end of your next long rest, at which point it vanishes to the afterlife. Once you bind a specter with this feature, you can't use the feature again until you finish a long rest.",
+            ],
+            level: 6,
+            tracked: true,
+            uses: 1,
+            recharge: "lr",
+          },
+          {
+            id: "armor_of_hexes",
+            name: "Armor of Hexes",
+            desc: [
+              "At 10th level, your hex grows more powerful. If the target cursed by your Hexblade's Curse hits you with an attack roll, you can use your reaction to roll a d6. On a 4 or higher, the attack instead misses you, regardless of its roll.",
+            ],
+            level: 10,
+            tracked: false,
+          },
+          {
+            id: "master_of_hexes",
+            name: "Master of Hexes",
+            desc: [
+              "Starting at 14th level, you can spread your Hexblade's Curse from a slain creature to another creature. When the creature cursed by your Hexblade's Curse dies, you can apply the curse to a different creature you can see within 30 feet of you, provided you aren't incapacitated.",
+              "When you apply the curse in this way, you don't regain hit points from the death of the previously cursed creature.",
+            ],
+            level: 14,
+            tracked: false,
+          },
+        ],
+        expandedSpellOptions: [
+          { spellLevel: 1, spells: ["shield", "wrathful-smite"] },
+          { spellLevel: 2, spells: ["blur", "branding-smite"] },
+          { spellLevel: 3, spells: ["blink", "elemental-weapon"] },
+          { spellLevel: 4, spells: ["phantasmal-killer", "staggering-smite"] },
+          { spellLevel: 5, spells: ["banishing-smite", "cone-of-cold"] },
+        ],
+      },
       undead: "",
       undying: "",
     },
