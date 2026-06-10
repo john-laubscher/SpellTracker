6f481934fdaa9c053b40b468824a36c1b9b2eb8c
John Laubscher <jlmemphiscommanders@gmail.com>
2026-06-09 22:04:05 -0500

    Expand wizard enchantment subclass descriptions

--- src/components/ClassesData.js
+++ src/components/ClassesData.js
@@ -7364,21 +7364,29 @@ export const ClassesData = {
           {
             id: "enchantment_savant",
             name: "Enchantment Savant",
-            desc: "The gold and time you spend to copy enchantment spells into your spellbook are halved.",
+            desc: "Beginning when you select this school at 2nd level, the gold and time you must spend to copy an Enchantment spell into your spellbook is halved.",
             level: 2,
             tracked: false,
           },
           {
             id: "hypnotic_gaze",
             name: "Hypnotic Gaze",
-            desc: "As an action, charm and daze a creature within 5 feet that can see or hear you. The target's speed becomes 0 and it is incapacitated until the end of your next turn, and you can sustain the effect with later actions.",
+            desc: [
+              "Starting at 2nd level when you choose this school, your soft words and enchanting gaze can magically enthrall another creature. As an action, choose one creature that you can see within 5 feet of you. If the target can see or hear you, it must succeed on a Wisdom saving throw against your wizard spell save DC or be charmed by you until the end of your next turn. The charmed creature's speed drops to 0, and the creature is incapacitated and visibly dazed.",
+              "On subsequent turns, you can use your action to maintain this effect, extending its duration until the end of your next turn. However, the effect ends if you move more than 5 feet away from the creature, if the creature can neither see nor hear you, or if the creature takes damage.",
+              "Once the effect ends, or if the creature succeeds on its initial saving throw against this effect, you can't use this feature on that creature again until you finish a long rest.",
+            ],
             level: 2,
             tracked: false,
           },
           {
             id: "instinctive_charm",
             name: "Instinctive Charm",
-            desc: "When a visible creature within 30 feet attacks you, you can use your reaction to try to redirect that attack toward another nearby creature unless the attacker succeeds on a Wisdom save.",
+            desc: [
+              "Beginning at 6th level, when a creature you can see within 30 feet of you makes an attack roll against you, you can use your reaction to divert the attack, provided that another creature is within the attack's range. The attacker must make a Wisdom saving throw against your wizard spell save DC. On a failed save, the attacker must target the creature that is closest to it, not including you or itself. If multiple creatures are closest, the attacker chooses which one to target.",
+              "On a successful save, you can't use this feature on the attacker again until you finish a long rest.",
+              "You must choose to use this feature before knowing whether the attack hits or misses. Creatures that can't be charmed are immune to this effect.",
+            ],
             level: 6,
             tracked: false,
           },
@@ -7392,7 +7400,10 @@ export const ClassesData = {
           {
             id: "alter_memories",
             name: "Alter Memories",
-            desc: "When you charm a creature with an enchantment spell, you can make one creature unaware it was charmed and can later try to erase part of the time it spent under the spell's influence.",
+            desc: [
+              "At 14th level, you gain the ability to make a creature unaware of your magical influence on it. When you cast an enchantment spell to charm one or more creatures, you can alter one creature's understanding so that it remains unaware of being charmed.",
+              "Additionally, once before the spell expires, you can use your action to try to make the chosen creature forget some of the time it spent charmed. The creature must succeed on an Intelligence saving throw against your wizard spell save DC or lose a number of hours of its memories equal to 1 + your Charisma modifier (minimum 1). You can make the creature forget less time, and the amount of time can't exceed the duration of your enchantment spell.",
+            ],
             level: 14,
             tracked: false,
           },
